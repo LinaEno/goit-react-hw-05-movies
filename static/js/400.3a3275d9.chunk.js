5a323d0a0625d2111c6090a58cbfdfd21d927a79
LinaEno <116013774+LinaEno@users.noreply.github.com>
2023-02-18 18:53:17 +0000

    Deploying to gh-pages from @ LinaEno/goit-react-hw-05-movies@46032333fc0a8255705b3d34dcc4c69a5f331ec6 🚀

--- static/js/400.3a3275d9.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[400],{7400:function(e,t,n){n.r(t);var r=n(2791),u=n(7689),c=n(184);t.default=function(){var e=(0,u.s0)();return(0,r.useEffect)((function(){var t=setTimeout((function(){e("/")}),5e3);return function(){return clearTimeout(t)}}),[e]),(0,c.jsx)("div",{children:(0,c.jsx)("p",{children:"Page not Found!"})})}}}]);
-//# sourceMappingURL=400.3a3275d9.chunk.js.map
